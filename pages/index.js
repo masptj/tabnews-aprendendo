79562bb01b90cef3137b4dae99a93e684283180a
Marcelino Pinheiro <masptj@gmail.com>
2025-07-29 16:08:51 +0000

    corrigido o arquivo index.js em pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 function Home () {
     return (
         <>
-        <h1>Olá estou vivo, agora para todos verem - Legal</h1> 
-        <p>Agora funcionou</p>
+        <h1>Sueli, amo você!</h1> 
         </>
     )
     
