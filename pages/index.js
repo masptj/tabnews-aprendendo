fc4e08d89800cb31c85a964c2fb61679b3ba50e6
Marcelino Pinheiro <masptj@gmail.com>
2025-07-29 16:04:58 +0000

    adicionado a pasta pages

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home () {
+    return (
+        <>
+        <h1>Olá estou vivo, agora para todos verem - Legal</h1> 
+        <p>Agora funcionou</p>
+        </>
+    )
+    
+}
+
+export default Home;
